e3084d9eaaccc6b5ab3aa48540ba68ccea363f05
Oscar Olsson <happybits@gmail.com>
2020-01-07 10:09:34 +0100

    Core, UI, Test project

---
+++ BadCopy.Core/Batch.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace BadCopy.Core
+{
+    public class Batch
+    {
+        public string Name { get; set; }
+        public List<string> FromFolders { get; set; }
+        public CopyStyle CopyStyle { get; set; }
+        public string ToFolder { get; set; }
+    }
+}
---
+++ BadCopy.UI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BadCopy.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Ta reda på vilka filer som ska kopieras 
+             - Ta hänsyn till historik? (för att slippa kopiera filen om det redan är gjort) 
+             - Utgå ifrån fil med mappnamn och rootnamn
+
+             Gå igenom alla filer. Kopiera en fil från en mapp till en annan:
+             - Skapa en mapp om det behövs
+             - Kopiera filen utan lösning, identisk eller utan #region #solution
+             -  
+            
+             
+             */
+            // Ange från mappar och till mappar
+            // 
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
