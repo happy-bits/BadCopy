250cb07431f915c4ad42ebe3a2605668e785d655
Oscar Olsson <happybits@gmail.com>
2020-01-30 15:44:27 +0100

    -

---
+++ BadCopy.Core.Test/TransformUtility_GetMethodsRow.cs
@@ -0,0 +1,49 @@
+﻿using BadCopy.Core.Transformations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadCopy.Core.Test
+{
+    [TestClass]
+    public class TransformUtility_GetMethodsRow
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            var x = new TransformUtility();
+
+            var s = @"
+
+                public List<int> Add100ToEachNumber(List<int> numbers)
+                {
+                    var result = new List<int>();
+                    foreach (var number in numbers)
+                    {
+                        int newNumber = number + 100;
+                        result.Add(newNumber);
+                    }
+
+                    return result;
+                }
+
+                public List<int> Add100ToEachNumber_Linq(List<int> numbers)
+                {
+                    return numbers.Select(x => x + 100).ToList();
+                }
+
+            ";
+
+            List<string> result =  x.GetMethodsRows(s);
+
+            CollectionAssert.AreEqual(new string[] {
+            "public List<int> Add100ToEachNumber(List<int> numbers)",
+            "public List<int> Add100ToEachNumber_Linq(List<int> numbers)"
+            }, result);
+
+        }
+    }
+}
---
+++ BadCopy.Core/Transformations/TransformUtility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BadCopy.Core.Transformations
+{
+    public class TransformUtility
+    {
+        public List<string> GetMethodsRows(string s)
+        {
+            //var result = Regex.Match(s, @"((\s*?)( *(public|private).*).*)+", RegexOptions.);
+            var result = new List<string>();
+
+            var splitt = s.Split("public");
+
+            for(var i=1; i<splitt.Length; i++)
+            {
+                result.Add("public" + splitt[i].Split('\n','\r')[0]);
+            }
+
+            return result;
+        }
+    }
+}
